27b0a687624cbf2460a5193241719118fc227655
Ginpei <ginpei@ginpei.dev>
2023-03-09 09:25:36 -0800

    refactor: 💡 replace OpenAI API over npm with web end point

--- src/domains/openai/chatRequestManipulators.ts
+++ src/domains/openai/chatRequestManipulators.ts
@@ -1,10 +1,27 @@
-import {
-  ChatApiErrorResponse,
-  chatApiPath,
-  ChatApiQuery,
-  ChatApiSuccessResponse,
-} from "../../../pages/api/chat";
+import type { CreateCompletionResponse } from "openai";
 
+export interface ChatApiQuery {
+  apiKey: string;
+  prompt: string;
+}
+
+export interface ChatApiSuccessResponse {
+  data: CreateCompletionResponse;
+  ok: true;
+}
+
+export interface CreateCompletionErrorResponse {
+  error: {
+    code: null;
+    message: string;
+    param: null;
+    type: string;
+  };
+}
+
+/**
+ * @see API Reference - OpenAI API https://platform.openai.com/docs/api-reference/completions
+ */
 export async function sendChatRequest(
   options: ChatApiQuery
 ): Promise<ChatApiSuccessResponse> {
@@ -12,19 +29,26 @@ export async function sendChatRequest(
     throw new Error("`apiKey` is required");
   }
 
+  const url = "https://api.openai.com/v1/completions";
   const init: RequestInit = {
-    body: JSON.stringify(options),
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: options.prompt,
+      temperature: 0.6,
+      max_tokens: 150,
+    }),
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${options.apiKey}`,
     },
     method: "POST",
   };
-  const res = await fetch(chatApiPath, init);
+  const res = await fetch(url, init);
   if (!res.ok) {
-    const errorBody: ChatApiErrorResponse = await res.json();
-    throw new Error(errorBody.message);
+    const data: CreateCompletionErrorResponse = await res.json();
+    throw new Error(`${data.error.message} (${data.error.type})`);
   }
 
-  const data: ChatApiSuccessResponse = await res.json();
-  return data;
+  const data: CreateCompletionResponse = await res.json();
+  return { data, ok: true };
 }
