7fc28c1adb7223a7c4dd062ad9070059b511c896
Ginpei <ginpei@ginpei.dev>
2023-03-10 02:29:19 -0800

    refactor: 💡 extract ChatControlBlock

---
+++ src/pages/home/ChatControlBlock.tsx
@@ -0,0 +1,97 @@
+import { useState } from "react";
+import { NiceButton } from "../../domains/button/NiceButton";
+import { useChatHistoryContext } from "../../domains/chat/ChatHistoryContext";
+import { loadChatLog, saveChatLog } from "../../domains/chat/chatLogStore";
+import { buildChatMessage } from "../../domains/chat/ChatMessage";
+import { buildPrompt } from "../../domains/chat/chatMessageManipulators";
+import { useError } from "../../domains/error/errorHooks";
+import { toError } from "../../domains/error/errorManipulators";
+import { VStack } from "../../domains/layout/VStack";
+import { useChatGptApiContext } from "../../domains/openai/chatGptApiContext";
+import { sendChatRequest } from "../../domains/openai/chatRequestManipulators";
+import { ChatForm } from "./ChatForm";
+import { ChatGptApiKeyForm } from "./ChatGptApiKeyForm";
+
+export interface ChatControlBlockProps {}
+
+export function ChatControlBlock({}: ChatControlBlockProps): JSX.Element {
+  const [apiContext] = useChatGptApiContext();
+  const [history, setHistory] = useChatHistoryContext();
+  const [sendError, setSendError] = useError();
+  const [requestMessage, setRequestMessage] = useState(
+    history.messages.length > 0 ? "" : "Say something funny"
+  );
+  const [processingChat, setProcessingChat] = useState(false);
+  const [toolsOpen] = useState(apiContext.apiKey === "" ? true : undefined);
+
+  const onSubmit = async () => {
+    setProcessingChat(true);
+    setSendError(null);
+    try {
+      const userMessage = buildChatMessage({
+        body: requestMessage,
+        name: "you",
+      });
+      const messageWithUserUpdate = [...history.messages, userMessage];
+      setHistory({ ...history, messages: messageWithUserUpdate });
+
+      const prompt = buildPrompt(messageWithUserUpdate);
+
+      const result = await sendChatRequest({
+        apiKey: apiContext.apiKey,
+        prompt,
+      });
+
+      // TODO
+      console.log("# result", result);
+
+      const aiMessage = buildChatMessage({
+        body: result.data.choices[0].text?.trim() ?? "?",
+        name: "ai",
+      });
+      setHistory((prevHistory) => {
+        const newMessages = [...prevHistory.messages, aiMessage];
+        saveChatLog(newMessages);
+        return { ...prevHistory, messages: newMessages };
+      });
+      setRequestMessage("");
+    } catch (error) {
+      console.error(error);
+      setSendError(toError(error));
+    } finally {
+      setProcessingChat(false);
+    }
+  };
+
+  const onClearHistoryClick = () => {
+    const ok = window.confirm("Are you sure you want to remove all chat log?");
+    if (!ok) {
+      return;
+    }
+    saveChatLog([]);
+    setHistory({ ...history, messages: [] });
+  };
+
+  return (
+    <div className="ChatControlBlock bg-gray-100 p-4">
+      <VStack gap="gap-2">
+        {sendError && <p className="text-red-700">{sendError.message}</p>}
+        <ChatForm
+          disabled={processingChat}
+          input={requestMessage}
+          onInputChange={setRequestMessage}
+          onSubmit={onSubmit}
+        />
+        <details open={toolsOpen}>
+          <summary>Tools</summary>
+          <div className="flex gap-2">
+            <ChatGptApiKeyForm />
+            <NiceButton onClick={onClearHistoryClick}>
+              🗑️ Clear history...
+            </NiceButton>
+          </div>
+        </details>
+      </VStack>
+    </div>
+  );
+}
--- src/pages/home/ChatSection.tsx
+++ src/pages/home/ChatSection.tsx
@@ -1,100 +1,15 @@
-import { useState } from "react";
-import { NiceButton } from "../../domains/button/NiceButton";
 import { useChatHistoryContext } from "../../domains/chat/ChatHistoryContext";
-import { saveChatLog } from "../../domains/chat/chatLogStore";
-import { buildChatMessage } from "../../domains/chat/ChatMessage";
-import { buildPrompt } from "../../domains/chat/chatMessageManipulators";
-import { useError } from "../../domains/error/errorHooks";
-import { toError } from "../../domains/error/errorManipulators";
-import { VStack } from "../../domains/layout/VStack";
-import { useChatGptApiContext } from "../../domains/openai/chatGptApiContext";
-import { sendChatRequest } from "../../domains/openai/chatRequestManipulators";
-import { ChatForm } from "./ChatForm";
-import { ChatGptApiKeyForm } from "./ChatGptApiKeyForm";
+import { ChatControlBlock } from "./ChatControlBlock";
 import { ChatItem } from "./ChatItem";
 
 export interface ChatSectionProps {}
 
 export function ChatSection(): JSX.Element {
-  const [apiContext] = useChatGptApiContext();
-  const [sendError, setSendError] = useError();
-  const [history, setHistory] = useChatHistoryContext();
-  const [requestMessage, setRequestMessage] = useState(
-    history.messages.length > 0 ? "" : "Say something funny"
-  );
-  const [processingChat, setProcessingChat] = useState(false);
-  const [toolsOpen] = useState(apiContext.apiKey === "" ? true : undefined);
-
-  const onSubmit = async () => {
-    setProcessingChat(true);
-    setSendError(null);
-    try {
-      const userMessage = buildChatMessage({
-        body: requestMessage,
-        name: "you",
-      });
-      const messageWithUserUpdate = [...history.messages, userMessage];
-      setHistory({ ...history, messages: messageWithUserUpdate });
-
-      const prompt = buildPrompt(messageWithUserUpdate);
-
-      const result = await sendChatRequest({
-        apiKey: apiContext.apiKey,
-        prompt,
-      });
-
-      // TODO
-      console.log("# result", result);
-
-      const aiMessage = buildChatMessage({
-        body: result.data.choices[0].text?.trim() ?? "?",
-        name: "ai",
-      });
-      setHistory((prevHistory) => {
-        const newMessages = [...prevHistory.messages, aiMessage];
-        saveChatLog(newMessages);
-        return { ...prevHistory, messages: newMessages };
-      });
-      setRequestMessage("");
-    } catch (error) {
-      console.error(error);
-      setSendError(toError(error));
-    } finally {
-      setProcessingChat(false);
-    }
-  };
-
-  const onClearHistoryClick = () => {
-    const ok = window.confirm("Are you sure you want to remove all chat log?");
-    if (!ok) {
-      return;
-    }
-    saveChatLog([]);
-    setHistory({ ...history, messages: [] });
-  };
+  const [history] = useChatHistoryContext();
 
   return (
     <div className="ChatSection flex flex-col-reverse content-between">
-      {sendError && <p className="text-red-700">{sendError.message}</p>}
-      <div className="bg-gray-100 p-4">
-        <VStack gap="gap-2">
-          <ChatForm
-            disabled={processingChat}
-            input={requestMessage}
-            onInputChange={setRequestMessage}
-            onSubmit={onSubmit}
-          />
-          <details open={toolsOpen}>
-            <summary>Tools</summary>
-            <div className="flex gap-2">
-              <ChatGptApiKeyForm />
-              <NiceButton onClick={onClearHistoryClick}>
-                🗑️ Clear history...
-              </NiceButton>
-            </div>
-          </details>
-        </VStack>
-      </div>
+      <ChatControlBlock />
       <div className="flex-grow overflow-auto">
         {history.messages.map((message) => (
           <ChatItem key={message.id} message={message} />
