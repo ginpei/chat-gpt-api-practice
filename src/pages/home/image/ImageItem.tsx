2c9665f7fbe38e3f2888a991a1266746a5a9783e
Ginpei <ginpei@ginpei.dev>
2023-04-09 12:52:29 -0700

    feat: 🎸 show image load failure

--- src/pages/home/image/ImageItem.tsx
+++ src/pages/home/image/ImageItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ImageGeneration } from "../../../domains/imageGeneration/ImageHistory";
 import { Container } from "../../../domains/layout/Container";
 
@@ -5,7 +6,23 @@ export interface ImageItemProps {
   image: ImageGeneration;
 }
 
+const svgNoImage = /* svg */ `
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'>
+    <text x='50%' y='50%' dominant-baseline='middle' text-anchor='middle' font-size='10' fill='#ccc'>
+      No image
+    </text>
+  </svg>
+`;
+
+const fallbackUrl = `data:image/svg+xml,${encodeURIComponent(svgNoImage)}`;
+
 export function ImageItem({ image }: ImageItemProps): JSX.Element {
+  const [errored, setErrored] = useState(false);
+
+  const onError = () => {
+    setErrored(true);
+  };
+
   return (
     <div className="ImageItem border-b py-4">
       <Container>
@@ -16,10 +33,16 @@ export function ImageItem({ image }: ImageItemProps): JSX.Element {
               <img
                 alt={image.url}
                 className="inline-block mx-auto w-full max-w-[50vh] border"
-                src={image.url}
+                onError={onError}
+                src={errored ? fallbackUrl : image.url}
               />
             </a>
           </div>
+          {errored && (
+            <p className="text-red-500">
+              Failed to load. Most likely the image has expired.
+            </p>
+          )}
           <p>
             <small className="text-stone-500 pr-1 select-none">Prompt:</small>
             {image.prompt}
